1967e0bd376e65ea0b9ba26fc4c97a64e2e39452
traoreera <traoreera@gmail.com>
2026-05-25 08:22:14 +0000

    fix swager ui

--- main.py
+++ main.py
@@ -40,6 +40,10 @@ async def lifespan(app: FastAPI):
 app = FastAPI(
     **xcore._config.app.fastapi.__dict__,
     lifespan=lifespan,
+    docs_url=None,
+    redoc_url=None,
+    openapi_url=None,
+    swagger_ui_oauth2_redirect_url=None,
 )
 
 # ── Middlewares (ordre LIFO : le dernier ajouté est exécuté en premier) ───────
